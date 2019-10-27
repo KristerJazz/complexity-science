67219f50db996db6e2b5e5c3f941bdc97c22bf20
Jazz <kristerjazz.urog@gmail.com>
2019-10-27 13:11:04 +0100

    Add initial set up for version 0.0.1

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name = "complexity-science",
+        version = "0.0.1",
+        author = "Krister Jazz Urog",
+        author_email = "kristerjazz.urog@gmail.com",
+        description = "A package for complexity science research",
+        long_description = long_description,
+        long_description_content_type = "text/markdown",
+        url = "https://github.com/complexity-science",
+        packages = setuptools.find_packages(),
+        classifiers = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires = '>3.6'
+        )
