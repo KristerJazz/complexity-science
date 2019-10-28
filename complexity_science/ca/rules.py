5fc55b2c6933e5d51e0f6bb4f19be8904db6f256
Jazz <kristerjazz.urog@gmail.com>
2019-10-29 00:06:07 +0100

    Add initial game of life rules

--- complexity_science/ca/rules.py
+++ complexity_science/ca/rules.py
@@ -159,6 +159,32 @@ class Bit128:
         result = np.logical_and(result, current_state).astype(int)
         return result
 
+class GameOfLife:
+    def apply(self, current, neighbors):
+        for key in neighbors:
+            total_neighbors += neighbors[keys]
+
+        result = np.zeros_like(current)
+        state = current.copy()
+
+        live = (current==1)
+        dead = (current==0)
+        two = (total_neighbors == 2)
+        three = (total_neighbors ==3)
+
+        less_two = (total_neighbors < 2)
+        more_three = (total_neighbors >3)
+        two_or_three = np.logical_or(two,three)
+
+        result += np.logical_and(live, two_or_three).astype(int)
+        result += np.logical_and(dead, three).astype(int)
+
+        state -= np.logical_and(live, less_two).astype(int)
+        state -= np.logical_and(live, more_three).astype(int)
+
+        result += state
+
+        return result
 """
 class RuleManager1D(RuleManager):
     def __init__(self):
