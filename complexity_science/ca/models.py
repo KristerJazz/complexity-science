c52d4eb283c677d8394776eba11d81ba56f1524e
Jazz <kristerjazz.urog@gmail.com>
2019-10-31 22:56:05 +0100

    Add MPA model

--- complexity_science/ca/models.py
+++ complexity_science/ca/models.py
@@ -1,7 +1,7 @@
 from .ca2d import *
 from .rules import *
 
-def briansbrain(dim, toroidal=True, default=True):
+def brians_brain(dim, toroidal=True, default=True):
     if toroidal:
         model = MOORE_CA_t(dim)
     else:
@@ -10,5 +10,28 @@ def briansbrain(dim, toroidal=True, default=True):
     if default:
         model.set_default(BriansBrain())
 
+    model.initialize_random_int(0,3)
     return model
+
+def game_of_life(dim, toroidal=True, default=True):
+    if toroidal:
+        model = MOORE_CA_t(dim)
+    else:
+        model = MOORE_CA(dim)
+
+    if default:
+        model.set_default(GameOfLife())
+
+    model.initialize_random_bin(0.5)
+    return model
+
+def mpa(dim, toroidal=True):
+    if toroidal:
+        model = VON_CA_t(dim)
+    else:
+        model = VON_CA(dim)
+
+    return model
+
+    model.initialize_random()
 
