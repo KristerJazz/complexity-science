666dd6e06e6aeed1da20dfba05d3c5d54be32fcc
Jazz <kristerjazz.urog@gmail.com>
2019-11-30 04:30:58 +0100

    Fix default rule for applause model

--- complexity_science/ca/models2d.py
+++ complexity_science/ca/models2d.py
@@ -2,6 +2,7 @@ from .ca.ca2d import *
 from .rules2d.brians import *
 from .rules2d.game_of_life import *
 from .rules2d.applause import *
+from .rules2d.mpa import *
 
 def brians_brain(dim, toroidal=True, default=True):
     if toroidal:
@@ -15,6 +16,7 @@ def brians_brain(dim, toroidal=True, default=True):
     model.initialize_random_int(0,3)
     return model
 
+
 def game(dim, toroidal=True, default=True):
     if toroidal:
         model = MooreCA_t(dim)
@@ -27,36 +29,39 @@ def game(dim, toroidal=True, default=True):
     model.initialize_random_bin(0.5)
     return model
 
-def applause(dim, rule_object='default', simple=True):
+
+def applause(dim, rule_object='default', simple=True, **kwargs):
     if simple:
         model = MooreCA_t(dim)
 
     if (rule_object=='default'):
-        params = {}
-        params['a'] = 0.5
-        params['b'] = 0.3
-        params['alpha'] = 1
-        params['beta'] = 5
-
-        print('Applause rule is set')
-        model.set_rule(Applause(params))
+        model.set_rule(Applause(**kwargs))
     else:
         model.set_rule(rule_object)
 
     model.initialize_random_bin(0.9)
-        print(self.cells)
-
     return model
 
-def mpa(dim, toroidal=True, params=None):
+
+def mpa(dim, rule_object='default', toroidal=True, **kwargs):
     if toroidal:
         model = VonCA_t(dim)
     else:
         model = VonCA(dim)
 
-    #if params:
-    #    model.
+    if rule_object=='default':
+        dt = 0.001
+        D = 30
+        beta = 0.8
+        #growth params
+        params = {}
+        params['dt'] = dt
+        params['beta'] = beta
+        params['D'] = D
+        params['alpha'] = D*dt
+
+    else:
+        model.set_rule(rule_object)
+
     model.initialize_random()
     return model
-
-
