a1a93437b60ff15c2d27571c3d5b6468078b08e3
Jazz <kristerjazz.urog@gmail.com>
2020-04-11 15:07:13 +0200

    Add sample gillespie model

--- complexity_science/ca/models_network.py
+++ complexity_science/ca/models_network.py
@@ -3,14 +3,14 @@ from .ca.ca_network import *
 from .rules_network.migration import *
 from .rules_network.gillespie import *
 
-def guillespie(adj, heterogeneity=4, rule='default'):
+def gillespie(adj, heterogeneity=4, rule='default'):
     model = CA_Network(adj, heterogeneity)
     if rule=='default':
-        model.add_rule(SIRC_Gillespie())
-        print("Added model")
+        model.set_rule(SIRC_Gillespie())
+        #model.add_rule(Migration())
     return model
 
 def migration(adj):
     model = CA_Network(adj,3)
-    model.add_rule(Migration())
+    model.set_rule(Migration())
     return model
