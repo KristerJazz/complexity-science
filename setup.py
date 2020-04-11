6794fe7e0bb0f56fb55ab7a5231db287920b3721
Jazz <kristerjazz.urog@gmail.com>
2020-04-11 15:21:11 +0200

    Update version number to 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name = "complexity-science",
-        version = "0.0.4",
+        version = "0.0.5",
         author = "Krister Jazz Urog",
         author_email = "kristerjazz.urog@gmail.com",
         description = "A package for complexity science research",
